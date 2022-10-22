b37db7cb11bed8c4a727ca5bb15e5fa158a1928f
samhayder <sams.seul@gmail.com>
2022-10-22 23:27:01 +0600

    OOP Basic

---
+++ src/ProblemSolving/ProblemSolving1/Box.java
@@ -0,0 +1,9 @@
+package OOP.ProblemSolving.ProblemSolving1;
+
+public class Box {
+     /*
+    Create a class called Box that include three pieces of information as instance variables-height, width and depth (type double) of two boxes. Your class should have a constructor and initialize the three instance variables. Provide a method displayVol that display the volume of two boxes. Suppose, the values of instance variables for the first box's are (10,10,10) and second box's are (20,30,10). Write a test application named boxVolume to demonstrate class Box's capabilities.
+    */
+
+
+}
